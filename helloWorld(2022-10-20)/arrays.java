eaae5e6717bc1f0411ee0a39a513442207d32b31
DaleWillemse <DaleWillemse000@gmail.com>
2022-10-22 19:48:20 +0200

    comments :)

--- helloWorld(2022-10-20)/arrays.java
+++ helloWorld(2022-10-20)/arrays.java
@@ -1,15 +1,16 @@
 public class arrays {
     public static void main(String[] args) {
+        // strings
         String[] cars = { "BMW", "Ford", "Nissan" };
-
+        // integers
         int[] numbers = { 5, 10, 15 };
-
+        // printing a specific array value
         System.out.println(cars[1]);
         System.out.println(numbers[0]);
-
+        // changing an array value
         cars[1] = "Merc";
         System.out.println(cars[1]);
-
+        // printing array length
         System.out.println(cars.length);
 
     }
