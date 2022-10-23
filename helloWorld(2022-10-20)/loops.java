3d18c21bd594f9424698a5a32ef38f82c97e45eb
DaleWillemse <DaleWillemse000@gmail.com>
2022-10-23 20:16:26 +0200

    love the foreach loop

---
+++ helloWorld(2022-10-20)/loops.java
@@ -0,0 +1,30 @@
+public class loops {
+    public static void main(String[] args) {
+
+        // while loop
+        int num = 0;
+        while (num < 3) {
+            System.out.println(num);
+            num++;
+        }
+
+        int donum = 0;
+        do {
+            System.out.println("do while");
+            donum++;
+        } while (donum < 3);
+
+        // for loop
+        for (int i = 1; i < 3; i++) {
+            System.out.println(i);
+        }
+
+        // for-each loop
+        String[] numbers = { "'1'", "'2'", "'3'" };
+
+        for (String i : numbers) {
+            System.out.println(i);
+        }
+
+    }
+}
