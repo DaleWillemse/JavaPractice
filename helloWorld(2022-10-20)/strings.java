d250c589c71c84d2e9b5fc035f89e8c53e32359a
DaleWillemse <DaleWillemse000@gmail.com>
2022-10-24 22:11:53 +0200

    strinnngs

---
+++ helloWorld(2022-10-20)/strings.java
@@ -0,0 +1,8 @@
+public class strings {
+    static void main(String[] args) {
+        String greeting = "hello";
+
+        System.out.println("The length of the string = " + greeting.length());
+        System.out.println(greeting.toUpperCase());
+    }
+}
