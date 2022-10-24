742edb30086be032cfa7b664d5fccf196e5c1ecc
DaleWillemse <DaleWillemse000@gmail.com>
2022-10-24 22:17:46 +0200

    need to do manipulation

--- helloWorld(2022-10-20)/strings.java
+++ helloWorld(2022-10-20)/strings.java
@@ -1,8 +1,11 @@
-public class strings {
-    static void main(String[] args) {
+class strings {
+    public static void main(String[] args) {
         String greeting = "hello";
 
         System.out.println("The length of the string = " + greeting.length());
         System.out.println(greeting.toUpperCase());
+        // toLowerCase() also works.
+
+        System.out.println(greeting.indexOf("e"));
     }
 }
