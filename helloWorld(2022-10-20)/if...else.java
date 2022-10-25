4edd44a9cbbcc4d2336162aac2e49d7953c41221
DaleWillemse <DaleWillemse000@gmail.com>
2022-10-25 21:29:14 +0200

    IF AND ELSE

---
+++ helloWorld(2022-10-20)/if...else.java
@@ -0,0 +1,14 @@
+class ifELse {
+    public static void main(String[] args) {
+        int count = 0;
+
+        if (count == 0) {
+            System.out.println("Count = 0");
+        } else if (count != 0) {
+            System.out.println("Count != 0");
+        } else {
+            System.out.println("Count isn't a number");
+        }
+    }
+
+}
