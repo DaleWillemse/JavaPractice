b81d1f9dd10691071819da88439ae28ec656f3c7
DaleWillemse <DaleWillemse000@gmail.com>
2022-10-25 21:53:23 +0200

    methooooods

--- bitMoreDifficult(2022-10-25)/methods.java
+++ bitMoreDifficult(2022-10-25)/methods.java
@@ -2,4 +2,18 @@ class methods {
     public static void main(String[] args) {
 
     }
+
+    void newMethod() { // creating a method.
+        // returns nothing. "void"
+    }
+
+    boolean returnMethod() { // returns type "boolean".
+        return false;
+    }
+
+    int fullMethod(int num) { // takes an int and stores it as "num", returns and int.
+        int answer = num + num;
+        return answer;
+    }
+
 }
