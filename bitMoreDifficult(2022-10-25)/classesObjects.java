832465fca67dd4e9ee723e7e53dbe276d946f3a9
DaleWillemse <DaleWillemse000@gmail.com>
2022-10-26 14:19:10 +0200

    classsses and objects!

---
+++ bitMoreDifficult(2022-10-25)/classesObjects.java
@@ -0,0 +1,8 @@
+public class classesObjects { // creating a new class.
+    int x = 5; // variable stored in the class.
+
+    public static void main(String[] args) {
+        classesObjects newObject = new classesObjects(); // creating a new object.
+        System.out.println(newObject.x); // calling the variable stored in the class, using the object.
+    }
+}
