3876bb89a5c3d14db7b543ed59b03263dc951792
DaleWillemse <DaleWillemse000@gmail.com>
2022-10-26 20:29:18 +0200

    .

---
+++ bitMoreDifficult(2022-10-25)/typeCasting.java
@@ -0,0 +1,13 @@
+public class typeCasting {
+    public static void main(String[] args) {
+        double aDoubleVar = 9.6d;
+
+        System.out.println(aDoubleVar);
+
+        System.out.println((int) aDoubleVar); // casting the double into an integer.
+
+        float aFloatVar = (float) aDoubleVar; // casting double var type into a var of type float.
+
+        System.out.println(aFloatVar);
+    }
+}
