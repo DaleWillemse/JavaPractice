ed57d6fbbaaa1cc90de7a8fd0f92f5612e7e8c75
DaleWillemse <DaleWillemse000@gmail.com>
2022-10-26 14:23:33 +0200

    more classsses.

--- bitMoreDifficult(2022-10-25)/classesObjects.java
+++ bitMoreDifficult(2022-10-25)/classesObjects.java
@@ -1,8 +1,15 @@
-public class classesObjects { // creating a new class.
+class NewClass { // creating another class within the same file.
+    int x = 4;
+}
+
+class ClassesObjects { // creating a new class.
     int x = 5; // variable stored in the class.
 
     public static void main(String[] args) {
-        classesObjects newObject = new classesObjects(); // creating a new object.
+        ClassesObjects newObject = new ClassesObjects(); // creating a new object.
         System.out.println(newObject.x); // calling the variable stored in the class, using the object.
+
+        NewClass secondObject = new NewClass();
+        System.out.println(secondObject.x); // creating a new object to call the class outside of this class.
     }
 }
