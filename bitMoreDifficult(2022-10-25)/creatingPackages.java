8f0a9239117332e07016c958fad96442f54a0346
DaleWillemse <DaleWillemse000@gmail.com>
2022-10-27 21:09:02 +0200

    packagesss

---
+++ bitMoreDifficult(2022-10-25)/creatingPackages.java
@@ -0,0 +1,7 @@
+package mypackage; // use "package" keyword to create a package.
+
+public class creatingPackages {
+    public static void main(String[] args) {
+        System.out.println("This is Dale's package.");
+    }
+}
---
+++ bitMoreDifficult(2022-10-25)/importingPackages.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; // use "import" keyword to use a package from any library.
+// Using "Scanner" class from the java.util package.
+
+// To use everything from a package, use a *.
+// e.g. "import java.util.*"
+
+public class importingPackages {
+    public static void main(String[] args) {
+
+        Scanner newObj = new Scanner(System.in); // Scanner allows for user input.
+        System.out.println("Enter your name:");
+
+        String yourName = newObj.nextLine(); // next is a method within Scanner, prints a new line.
+        System.out.println("Hi " + yourName);
+
+    }
+}
