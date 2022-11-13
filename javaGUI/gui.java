abd0fda5393f6cc2b5c2f74ccd2fcac8a978905d
Dale Willemse <DaleWillemse000@gmail.com>
2022-11-13 19:23:59 +0200

    Guuuuui

---
+++ javaGUI/gui.java
@@ -0,0 +1,50 @@
+package javaGUI;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.*;
+
+public class gui implements ActionListener {
+
+    private int count = 0;
+
+    private JFrame frame;
+    private JLabel lbl;
+    private JPanel panel;
+    private JButton btn;
+
+    public gui() {
+        frame = new JFrame();
+
+        btn = new JButton("Click Me");
+        btn.addActionListener(this);
+
+        lbl = new JLabel("Clicks: 0");
+
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(0, 1));
+        panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
+        panel.add(btn);
+        panel.add(lbl);
+
+        frame.add(panel, BorderLayout.CENTER); // Adds the panel to the screen (the window).
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close the application when closing the window.
+        frame.setTitle("First GUI");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new gui();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        lbl.setText("Clicks: " + count);
+    }
+}
